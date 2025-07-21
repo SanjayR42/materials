2b0d931af054cd2c0457c51cce44232422bcedbf
SanjayR42 <sanjayraghupathy1412@gmail.com>
2025-07-21 09:08:34 +0530

    First commit V2

---
+++ script.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>working with version 1.0</title>
+		<script>
+			// This script runs when the page loads
+			window.onload = function () {
+				alert("Welcome to Wipro Git Commands page!");
+			};
+		</script>
+	</head>
+	<body>
+		wipro git commands<br>
+		Welcome to my page!
+	</body>
+</html>
